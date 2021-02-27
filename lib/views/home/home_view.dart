21f2ccf988b6fc41abfb9e4fc9857e3f1d4ad9c7
Dustin Groh <dugroh@iu.edu>
2021-02-26 19:59:35 -0500

    updated home screen, added starter navigation

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -11,10 +11,50 @@ class HomeView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Image.asset('assets/images/logo.png'),
+        toolbarHeight: 80,
+        backgroundColor: Colors.red[700],
+        ),
+        drawer: Drawer(
+  elevation: 16.0,
+  child: Column(
+    children: <Widget>[
+      UserAccountsDrawerHeader(
+        accountName: Text("xyz"),
+        accountEmail: Text("xyz@gmail.com"),
+        currentAccountPicture: CircleAvatar(
+          backgroundColor: Colors.white,
+          child: Text("xyz"),
+        ),
+        
+      ),
+      ListTile(
+        title: new Text("Courses"),
+        leading: new Icon(Icons.mail),
+      ),
+      Divider(
+        height: 0.1,
+      ),
+      ListTile(
+        title: new Text("Primary"),
+        leading: new Icon(Icons.inbox),
+      ),
+      ListTile(
+        title: new Text("Social"),
+        leading: new Icon(Icons.people),
+      ),
+      ListTile(
+        title: new Text("Promotions"),
+        leading: new Icon(Icons.local_offer),
+      )
+    ],
+  ),
+),
       body: CenteredView(
         child: Column(
           children: <Widget>[
-            NavigationBar(),
+            //NavigationBar(),
             Expanded(
               child: Row(
                 children: <Widget>[
